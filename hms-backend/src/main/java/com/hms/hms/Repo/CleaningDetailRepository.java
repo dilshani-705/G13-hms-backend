81f4203ccc029e4ff594b926119720fad0778a26
Sandadinu Bimsara <127046415+Sandadinu@users.noreply.github.com>
2024-06-11 00:49:04 +0530

    Back-end of Complaints,cleaning and maintenance

---
+++ hms-backend/src/main/java/com/hms/hms/Repo/CleaningDetailRepository.java
@@ -0,0 +1,7 @@
+package com.hms.hms.Repo;
+
+import com.hms.hms.Entity.CleaningDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CleaningDetailRepository extends JpaRepository<CleaningDetail, Long> {
+}
---
+++ hms-backend/src/main/java/com/hms/hms/Repo/MaintenanceDetailRepository.java
@@ -0,0 +1,8 @@
+package com.hms.hms.Repo;
+
+import com.hms.hms.Entity.MaintenanceDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MaintenanceDetailRepository extends JpaRepository<MaintenanceDetail, Long> {
+
+}
