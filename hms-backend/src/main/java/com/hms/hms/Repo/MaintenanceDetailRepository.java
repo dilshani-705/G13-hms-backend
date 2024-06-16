17d84ff49cd9e21d263ec68a1e7466e163724d7c
Shehan Hirudika <127170082+ShehanHirudika@users.noreply.github.com>
2024-06-16 17:28:44 +0530

    Revert "Back-end of Complaints,cleaning and maintenance"

--- hms-backend/src/main/java/com/hms/hms/Repo/MaintenanceDetailRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hms.hms.Repo;
-
-import com.hms.hms.Entity.MaintenanceDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MaintenanceDetailRepository extends JpaRepository<MaintenanceDetail, Long> {
-
-}
