5e2275fa16a63b171c0e0b764c06e3d94d4ac5e4
chamodi-amanda-jayasuriya <chamodi_2020681@fot.ruh.ac.lk>
2024-07-01 13:03:04 +0530

    change password part

--- hms-backend/src/main/java/com/hms/hms/User/UserController/UsersController.java
+++ hms-backend/src/main/java/com/hms/hms/User/UserController/UsersController.java
@@ -1,5 +1,6 @@
 package com.hms.hms.User.UserController;
 
+import com.hms.hms.ChangePassword.PasswordDto;
 import com.hms.hms.Login.LoginDto;
 import com.hms.hms.Login.LoginMessage;
 import com.hms.hms.User.UserDataTransferObject.UserDto;
@@ -7,6 +8,7 @@ import com.hms.hms.User.UserEntity.User;
 import com.hms.hms.User.UserService.UsersService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -42,6 +44,21 @@ public class UsersController {
         UserDto updatedUser = usersService.updateUser(user_id, userDto);
         return ResponseEntity.ok(updatedUser);
     }
+
+    @PutMapping("/{userId}/changePassword")
+    public ResponseEntity<?> changePassword(@PathVariable("userId") String userId, @RequestBody PasswordDto passwordDto) {
+        try {
+            boolean isUpdated = usersService.changePassword(userId, passwordDto.getCurrentPassword(), passwordDto.getNewPassword());
+            if (isUpdated) {
+                return ResponseEntity.ok("Password updated successfully");
+            } else {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Failed to update password");
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("An error occurred");
+        }
+    }
+
     // Delete user by ID
     @DeleteMapping("/{userId}")
     public ResponseEntity<String> deleteUser(@PathVariable("userId") String user_id) {
